483c666d293c6a72111fae9a367c48ff67fa2dda
Harsh <harsh.pandey1357@gmail.com>
2023-04-27 12:04:14 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
       <Navbar />
       <div
         className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center"
-        style={{ height: "100vh" }}
+        style={{ height: "85.3vh" }}
       >
         <div className="flex lg:w-4/5 sm:mb-2 -mx-40">
           <form
@@ -45,10 +45,8 @@ function Contact() {
             >
               Wanna reach out?
             </h2>
-            <p className="leading relaxed mb-5 ">
-              Want to work on cool projects with me?
-              <br /> Please reach out by filling out this form <br />
-              and I will revert back within a business day!
+            <p className="leading relaxed mb-5" >
+              I will revert back within a business day!
             </p>
             {/* <div className="row" style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}> */}
             <div className="relative mb-4" style={{ width: "100%" }}>
@@ -89,7 +87,7 @@ function Contact() {
                 id="message"
                 name="message"
                 onChange={(e) => setMessage(e.target.value)}
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                className="w-full h-3/5 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               />
             </div>
             <button
