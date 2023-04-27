f2be1ce808275c90c911aa0b363ca13815709807
Harsh <harsh.pandey1357@gmail.com>
2023-04-27 11:37:12 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import About from './components/About'
 
 export const Home = () => {
   return (
-    <div style={{height:"100dvh"}}>
+    <div style={{height:"120dvh"}}>
         <Navbar/>
         <About/>
     </div>
