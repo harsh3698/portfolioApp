69795e596d1ef98c01e71e40b69b73a10ba5adfc
Harsh <harsh.pandey1357@gmail.com>
2023-04-27 11:36:25 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             Harsh
           </a>
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <Link className="mr-5 hover:text-white" to="/projects">
             Projects
           </Link>
