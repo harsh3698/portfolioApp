7c51fc0e66d971c9c7cbb959ac040062e9a3aae6
Harsh <harsh.pandey1357@gmail.com>
2023-04-27 11:35:50 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font min-h-screen">
       <Navbar/>
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container px-10 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
