688fb03a913dd5e85477254be71f42e6f69c62f7
Harsh <harsh.pandey1357@gmail.com>
2023-04-27 11:48:29 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -94,7 +94,7 @@ function Contact() {
             </div>
             <button
               type="submit"
-              style={{ width: "50%" }}
+              style={{ width: "100%" }}
               className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
             >
               Submit
